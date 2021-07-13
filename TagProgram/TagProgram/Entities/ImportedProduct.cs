908d3203edf2692018300fa5ca92824fcf78c983
DevCarlosOli <74160616+DevCarlosOli@users.noreply.github.com>
2021-07-12 23:10:19 -0300

    TagProgram
    This program performs a basic action with output data from a product.
    
    Este programa executa uma ação básica com dados de saída de um produto.

---
+++ TagProgram/TagProgram/Entities/ImportedProduct.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace TagProgram.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomFee { get; set; }
+
+        public ImportedProduct()
+        {
+
+        }
+
+        public ImportedProduct(string name, double price, double customFee) : base(name, price)
+        {
+            CustomFee = customFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomFee;
+        }
+
+        public override string PriceTag()
+        {
+            return Name + " $ " + TotalPrice().ToString("F2", CultureInfo.InvariantCulture) + " (Customs Fee: $ " + CustomFee.ToString("F2", CultureInfo.InvariantCulture) + ").";
+        }
+    }
+}
---
+++ TagProgram/TagProgram/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using TagProgram.Entities;
+
+namespace TagProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            Console.WriteLine("--> TAG PRICE <--");
+            Console.WriteLine();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, Used or Imported - [c / u / i]: ");
+                char choice = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+
+                if(choice == 'c')
+                {
+                    products.Add(new Product(name, price));                   
+                }
+                else if(choice == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    products.Add(new UsedProduct(name, price, date));
+                }
+                else
+                {
+                    Console.Write("Custom Fee: ");
+                    double customfee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    products.Add(new ImportedProduct(name, price, customfee));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS");
+            foreach(Product prod in products)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+
+        }
+    }
+}
